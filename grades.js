35e58393e33ca8994374e4c5d81f9ad519cd35ae
zedsalim <salimzaiidi@gmail.com>
2023-10-31 11:16:44 +0100

    fixing the Grades list

--- grades.js
+++ grades.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var gradesLink = document.querySelector("#footer .grades-link");
-    var gradesList = gradesLink.querySelector(".grades-list");
-    var gradeItems = gradesList.querySelectorAll("table tr:not(:first-child)");
-  
-    gradesList.style.display = "none";
-  
-    gradesLink.addEventListener("mouseover", function() {
+  var gradesLink = document.querySelector("#footer .grades-link");
+  var gradesList = gradesLink.querySelector(".grades-list");
+  var gradeItems = gradesList.querySelectorAll("table tr:not(:first-child");
+
+  gradesList.style.display = "none";
+
+  gradesLink.addEventListener("mouseover", function() {
+    if (window.innerWidth >= 768) {
       gradesList.style.display = "table";
-    });
-  
-    gradesLink.addEventListener("mouseout", function() {
-      gradesList.style.display = "none";
-    });
+    }
   });
+
+  gradesLink.addEventListener("click", function(e) {
+    if (window.innerWidth < 768) {
+      e.preventDefault();
+      gradesList.style.display = (gradesList.style.display === "table") ? "none" : "table";
+    }
+  });
+});
+
   
